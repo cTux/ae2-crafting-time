0267302bd554ce2f144e5f81e831b9135625a9af
Viacheslav Miroshko <ccctux@gmail.com>
2026-06-21 15:25:40 +0300

    fix: use ae2craftingtime config filename

--- versions/1.20.1/src/main/java/com/ctux/ae2craftingtime/mc1201/Ae2CraftingTime.java
+++ versions/1.20.1/src/main/java/com/ctux/ae2craftingtime/mc1201/Ae2CraftingTime.java
@@ -9,9 +9,10 @@ import net.minecraftforge.fml.config.ModConfig;
 @Mod(Ae2CraftingTime.MOD_ID)
 public final class Ae2CraftingTime {
     public static final String MOD_ID = "ae2craftingtime";
+    public static final String COMMON_CONFIG_FILE = "ae2craftingtime-common.toml";
 
     public Ae2CraftingTime() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Ae2CraftingTimeConfig.SPEC);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Ae2CraftingTimeConfig.SPEC, COMMON_CONFIG_FILE);
         StatsNetwork.register();
         MinecraftForge.EVENT_BUS.addListener(this::onServerStarted);
     }
