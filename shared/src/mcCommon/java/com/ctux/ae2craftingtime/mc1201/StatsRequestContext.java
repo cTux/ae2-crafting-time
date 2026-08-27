40e9f9cb50f1fd75aa382d76564a84fce15e8e29
Viacheslav Miroshko <ccctux@gmail.com>
2026-08-27 21:03:51 +0300

    refactor: access crafting menu state through mixin

--- shared/src/mcCommon/java/com/ctux/ae2craftingtime/mc1201/StatsRequestContext.java
+++ shared/src/mcCommon/java/com/ctux/ae2craftingtime/mc1201/StatsRequestContext.java
@@ -4,12 +4,16 @@ import appeng.api.networking.IGrid;
 import appeng.api.networking.security.IActionHost;
 import appeng.menu.AEBaseMenu;
 import appeng.menu.me.crafting.CraftingCPUMenu;
+import com.ctux.ae2craftingtime.mc1201.mixin.CraftingCPUMenuAccessor;
 import net.minecraft.server.level.ServerPlayer;
 
 public record StatsRequestContext(IGrid grid, Object craftingCpu) {
     public static StatsRequestContext current(ServerPlayer player) {
         if (player.containerMenu instanceof CraftingCPUMenu menu) {
-            return new StatsRequestContext(craftingCpuGrid(menu), craftingCpu(menu));
+            var accessor = (CraftingCPUMenuAccessor) menu;
+            var cpu = accessor.ae2craftingtime$getCpu();
+            return new StatsRequestContext(accessor.ae2craftingtime$getGrid(),
+                    cpu != null ? cpu : optionalAdvancedCpu(menu));
         }
         if (player.containerMenu instanceof AEBaseMenu menu && menu.getTarget() instanceof IActionHost host) {
             var node = host.getActionableNode();
@@ -18,24 +22,9 @@ public record StatsRequestContext(IGrid grid, Object craftingCpu) {
         return new StatsRequestContext(null, null);
     }
 
-    private static IGrid craftingCpuGrid(CraftingCPUMenu menu) {
+    private static Object optionalAdvancedCpu(CraftingCPUMenu menu) {
         try {
-            var method = CraftingCPUMenu.class.getDeclaredMethod("getGrid");
-            method.setAccessible(true);
-            return (IGrid) method.invoke(menu);
-        } catch (ReflectiveOperationException e) {
-            return null;
-        }
-    }
-
-    private static Object craftingCpu(CraftingCPUMenu menu) {
-        var cpu = craftingCpu(menu, "cpu");
-        return cpu != null ? cpu : craftingCpu(menu, "advancedAE$advCpu");
-    }
-
-    private static Object craftingCpu(CraftingCPUMenu menu, String fieldName) {
-        try {
-            var field = CraftingCPUMenu.class.getDeclaredField(fieldName);
+            var field = CraftingCPUMenu.class.getDeclaredField("advancedAE$advCpu");
             field.setAccessible(true);
             return field.get(menu);
         } catch (ReflectiveOperationException e) {
