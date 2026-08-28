8923483b5bed30ac3885c4972eeff9eb84f0de8b
Viacheslav Miroshko <ccctux@gmail.com>
2026-08-28 09:58:20 +0300

    fix(forge): use runtime-compatible channel id

--- versions/1.20.1-forge/src/main/java/com/ctux/ae2craftingtime/mc1201/StatsNetwork.java
+++ versions/1.20.1-forge/src/main/java/com/ctux/ae2craftingtime/mc1201/StatsNetwork.java
@@ -9,12 +9,11 @@ import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
 
-import java.util.Objects;
-
+@SuppressWarnings("deprecation")
 public final class StatsNetwork {
     private static final String PROTOCOL = "5";
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
-            Objects.requireNonNull(ResourceLocation.tryBuild(Ae2CraftingTime.MOD_ID, "main")),
+            new ResourceLocation(Ae2CraftingTime.MOD_ID, "main"),
             () -> PROTOCOL,
             PROTOCOL::equals,
             PROTOCOL::equals);
